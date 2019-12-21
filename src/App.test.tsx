a65fbdeef01f5e962653a1dfc63bdd0252042ac6
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-21 09:27:48 +0100

    Fixed test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders 2 rectangles', () => {
   const { getAllByTestId } = render(<App />);
   const rects = getAllByTestId("rect");
   expect(rects).toHaveLength(2);
