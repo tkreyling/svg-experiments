ec0fc5d75919f5b3395f49b56da5ae6c01c81066
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-21 09:25:28 +0100

    Adapted test to match visual components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ type RectProps = {
 
 const Rect: React.FC<RectProps> = (props) => {
   return (
-    <rect x={props.x} y={props.y} width="300" height="100" fill="rgb(0,0,255)" strokeWidth={3} stroke="rgb(0,0,0)" />
+    <rect data-testid="rect" x={props.x} y={props.y} width="300" height="100" fill="rgb(0,0,255)" strokeWidth={3} stroke="rgb(0,0,0)" />
   );
 };
 
