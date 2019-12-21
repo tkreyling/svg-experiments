eb4773a0f78adf1ecfb06541a72634e84d46771e
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-21 08:55:17 +0100

    Remove circle and path

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <svg viewBox="0 0 850 600">
-        <circle cx={50} cy={50} r={10} fill="red" />
-        <path d="M 0 0 L 100 100 L 150 100"
-          stroke="orange"
-          strokeWidth={1}
-          fill="none"
-        />
         <Rect x={150} y={150}/>
         <Rect x={250} y={260}/>
       </svg>
