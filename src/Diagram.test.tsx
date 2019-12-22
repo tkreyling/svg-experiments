22befb33c8f6580da8d09a86f22f70bd98c6df66
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-22 19:53:06 +0100

    Fix not compiling test because of previous refactoring

--- src/Diagram.test.tsx
+++ src/Diagram.test.tsx
@@ -6,7 +6,11 @@ test('renders 7 rectangles', () => {
   const layers = [
     ["element 1", "element 2", "an element with long text", "element 4"],
     ["element 1", "element 2", "element 3"]
-  ];
+  ].map(layer => {
+    return layer.map(name => {
+      return {name: name}
+    })
+  });
   const { getAllByTestId } = render(<Diagram layers={layers} />);
   const rects = getAllByTestId("rect");
   expect(rects).toHaveLength(7);
