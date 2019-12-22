298704c010229fdb4ed6be3d72b8331b6403d1b1
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-22 20:32:30 +0100

    Fix not compiling test because of previous refactoring

--- src/layoutHorizontally.test.tsx
+++ src/layoutHorizontally.test.tsx
@@ -1,24 +1,23 @@
 import {ELEMENT_WIDTH, HORIZONTAL_SPACING, layoutHorizontally, MARGIN_SIDE, MARGIN_TOP, widthOfElements} from "./App";
 
 test('no element results in no layouted elements', () => {
-    let elements: string[] = [];
-    expect(layoutHorizontally(elements, 0, widthOfElements(elements)))
+    expect(layoutHorizontally([], 0, widthOfElements([])))
       .toStrictEqual([])
 });
 
 test('one element is layouted to the origin', () => {
-    let elements = ["element"];
+    let elements = ["element"].map(name => ({name: name}));
     expect(layoutHorizontally(elements, 0, widthOfElements(elements)))
       .toStrictEqual([
-        {x: MARGIN_SIDE, y: MARGIN_TOP, element: "element", key: "0_0"}
+        {x: MARGIN_SIDE, y: MARGIN_TOP, element: {name: "element"}, key: "0_0"}
       ])
 });
 
 test('two elements are layouted right beside each other', () => {
-    let elements = ["element", "element"];
+    let elements = ["element", "element"].map(name => ({name: name}));
     expect(layoutHorizontally(elements, 0, widthOfElements(elements)))
       .toStrictEqual([
-        {x: MARGIN_SIDE, y: MARGIN_TOP, element: "element", key: "0_0"},
-        {x: MARGIN_SIDE + ELEMENT_WIDTH + HORIZONTAL_SPACING, y: MARGIN_TOP, element: "element", key: "0_1"}
+        {x: MARGIN_SIDE, y: MARGIN_TOP, element: {name: "element"}, key: "0_0"},
+        {x: MARGIN_SIDE + ELEMENT_WIDTH + HORIZONTAL_SPACING, y: MARGIN_TOP, element: {name: "element"}, key: "0_1"}
       ])
 });
