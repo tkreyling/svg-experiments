3e57131c453eb04fc01363a016eda2f8322f94e0
Thomas Kreyling <thomas.kreyling@googlemail.com>
2019-12-31 12:19:10 +0100

    Fix test build by adding missing relativePosition property

--- src/addConnectionIndexAndNumberOfEdges.test.tsx
+++ src/addConnectionIndexAndNumberOfEdges.test.tsx
@@ -7,8 +7,8 @@ test('no edges need no connection index', () => {
 test('one edge has connectionIndex 0 and the node has numberOfEdges 1', () => {
     let edges = [
         {
-            from: {key: "0_0", index: 0, layerIndex: 0},
-            to: {key: "1_0", index: 0, layerIndex: 1}
+            from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0},
+            to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1}
         }
     ];
 
@@ -17,9 +17,9 @@ test('one edge has connectionIndex 0 and the node has numberOfEdges 1', () => {
     expect(edges)
         .toStrictEqual([
             {
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 fromIndex: 0,
-                to: {key: "1_0", index: 0, layerIndex: 1, upperSideEdges: 1},
+                to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 toIndex: 0
             }
         ]);
@@ -28,8 +28,8 @@ test('one edge has connectionIndex 0 and the node has numberOfEdges 1', () => {
 test('an edge from a lower to an upper layer has connectionIndex 0 and the node has numberOfEdges 1', () => {
     let edges = [
         {
-            from: {key: "1_0", index: 0, layerIndex: 1},
-            to: {key: "0_0", index: 0, layerIndex: 0}
+            from: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1},
+            to: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0}
         }
     ];
 
@@ -38,9 +38,9 @@ test('an edge from a lower to an upper layer has connectionIndex 0 and the node 
     expect(edges)
         .toStrictEqual([
             {
-                from: {key: "1_0", index: 0, layerIndex: 1, upperSideEdges: 1},
+                from: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 fromIndex: 0,
-                to: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 1},
+                to: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             }
         ]);
@@ -49,8 +49,8 @@ test('an edge from a lower to an upper layer has connectionIndex 0 and the node 
 test('an edge on the same layer has two times lowerSideEdges = 1', () => {
     let edges = [
         {
-            from: {key: "0_0", index: 0, layerIndex: 0},
-            to: {key: "0_1", index: 1, layerIndex: 0}
+            from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0},
+            to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0}
         }
     ];
 
@@ -59,9 +59,9 @@ test('an edge on the same layer has two times lowerSideEdges = 1', () => {
     expect(edges)
         .toStrictEqual([
             {
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 fromIndex: 0,
-                to: {key: "0_1", index: 1, layerIndex: 0, lowerSideEdges: 1},
+                to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             }
         ]);
@@ -69,15 +69,15 @@ test('an edge on the same layer has two times lowerSideEdges = 1', () => {
 
 test('the second edge from one node will get an increased index and ' +
     'edges are sorted by the index of the other node in the layer', () => {
-    let node = {key: "0_0", index: 0, layerIndex: 0};
+    let node = {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0};
     let edges = [
         {
             from: node,
-            to: {key: "1_0", index: 0, layerIndex: 1}
+            to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1}
         },
         {
             from: node,
-            to: {key: "1_1", index: 1, layerIndex: 1}
+            to: {key: "1_1", index: 1, relativePosition: 0, layerIndex: 1}
         }
     ];
 
@@ -87,29 +87,29 @@ test('the second edge from one node will get an increased index and ' +
         .toStrictEqual([
             {
                 fromIndex: 0,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "1_0", index: 0, layerIndex: 1, upperSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 toIndex: 0
             },
             {
                 fromIndex: 1,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "1_1", index: 1, layerIndex: 1, upperSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "1_1", index: 1, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 toIndex: 0
             }
         ]);
 });
 
 test('edges to the same layer are sorted reversely', () => {
-    let node = {key: "0_0", index: 0, layerIndex: 0};
+    let node = {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0};
     let edges = [
         {
             from: node,
-            to: {key: "0_1", index: 1, layerIndex: 0}
+            to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0}
         },
         {
             from: node,
-            to: {key: "0_2", index: 2, layerIndex: 0}
+            to: {key: "0_2", index: 2, relativePosition: 0, layerIndex: 0}
         }
     ];
 
@@ -119,29 +119,29 @@ test('edges to the same layer are sorted reversely', () => {
         .toStrictEqual([
             {
                 fromIndex: 1,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "0_1", index: 1, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             },
             {
                 fromIndex: 0,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "0_2", index: 2, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "0_2", index: 2, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             }
         ]);
 });
 
 test('edges to the same layer are sorted after edges to another layer, if they are after the origin node', () => {
-    let node = {key: "0_0", index: 0, layerIndex: 0};
+    let node = {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0};
     let edges = [
         {
             from: node,
-            to: {key: "0_1", index: 1, layerIndex: 0}
+            to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0}
         },
         {
             from: node,
-            to: {key: "1_0", index: 0, layerIndex: 1}
+            to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1}
         }
     ];
 
@@ -151,29 +151,29 @@ test('edges to the same layer are sorted after edges to another layer, if they a
         .toStrictEqual([
             {
                 fromIndex: 1,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "0_1", index: 1, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             },
             {
                 fromIndex: 0,
-                from: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "1_0", index: 0, layerIndex: 1, upperSideEdges: 1},
+                from: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 toIndex: 0
             }
         ]);
 });
 
 test('edges to the same layer are sorted before edges to another layer, if they are before the origin node', () => {
-    let node = {key: "0_1", index: 1, layerIndex: 0};
+    let node = {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0};
     let edges = [
         {
             from: node,
-            to: {key: "0_0", index: 0, layerIndex: 0}
+            to: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0}
         },
         {
             from: node,
-            to: {key: "1_0", index: 0, layerIndex: 1}
+            to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1}
         }
     ];
 
@@ -183,14 +183,14 @@ test('edges to the same layer are sorted before edges to another layer, if they 
         .toStrictEqual([
             {
                 fromIndex: 0,
-                from: {key: "0_1", index: 1, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "0_0", index: 0, layerIndex: 0, lowerSideEdges: 1},
+                from: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "0_0", index: 0, relativePosition: 0, layerIndex: 0, lowerSideEdges: 1},
                 toIndex: 0
             },
             {
                 fromIndex: 1,
-                from: {key: "0_1", index: 1, layerIndex: 0, lowerSideEdges: 2},
-                to: {key: "1_0", index: 0, layerIndex: 1, upperSideEdges: 1},
+                from: {key: "0_1", index: 1, relativePosition: 0, layerIndex: 0, lowerSideEdges: 2},
+                to: {key: "1_0", index: 0, relativePosition: 0, layerIndex: 1, upperSideEdges: 1},
                 toIndex: 0
             }
         ]);
