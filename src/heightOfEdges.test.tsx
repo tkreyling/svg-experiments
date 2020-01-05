58f8e46ff400b44d617ef82d310abd5a450c95da
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-01-05 12:16:34 +0100

    Unify indentation

--- src/heightOfEdges.test.tsx
+++ src/heightOfEdges.test.tsx
@@ -1,13 +1,13 @@
 import {EDGE_SPACING, heightOfEdges} from "./App";
 
 test('no edges need no additional space after a layer', () => {
-  expect(heightOfEdges([], 1))
-      .toStrictEqual([0])
+    expect(heightOfEdges([], 1))
+        .toStrictEqual([0])
 });
 
 test('no edges need no space between layers', () => {
-  expect(heightOfEdges([], 3))
-      .toStrictEqual([0, 0, 0])
+    expect(heightOfEdges([], 3))
+        .toStrictEqual([0, 0, 0])
 });
 
 test('one edge between two consecutive layers requires no additional space', () => {
@@ -38,40 +38,39 @@ test('two edges between two consecutive layers require additional spacing', () =
 });
 
 test('an edge from a lower layer to an upper layer requires space below the upper layer', () => {
-  expect(heightOfEdges([
-    {
-        key: "0_0", index: 0, layerIndex: 0, relativePosition: 0,
-      from: {key: "1_0", index: 0, layerIndex: 1, relativePosition: 0},
-      to: {key: "0_0", index: 0, layerIndex: 0, relativePosition: 0}
-    },
-    {
-        key: "0_0", index: 1, layerIndex: 0, relativePosition: 0,
-      from: {key: "1_1", index: 1, layerIndex: 1, relativePosition: 0},
-      to: {key: "0_1", index: 1, layerIndex: 0, relativePosition: 0}
-    }
-  ], 2))
-      .toStrictEqual([EDGE_SPACING, 0])
+    expect(heightOfEdges([
+        {
+            key: "0_0", index: 0, layerIndex: 0, relativePosition: 0,
+            from: {key: "1_0", index: 0, layerIndex: 1, relativePosition: 0},
+            to: {key: "0_0", index: 0, layerIndex: 0, relativePosition: 0}
+        },
+        {
+            key: "0_0", index: 1, layerIndex: 0, relativePosition: 0,
+            from: {key: "1_1", index: 1, layerIndex: 1, relativePosition: 0},
+            to: {key: "0_1", index: 1, layerIndex: 0, relativePosition: 0}
+        }
+    ], 2))
+        .toStrictEqual([EDGE_SPACING, 0])
 });
 
 test('edges from the bottom layer to the bottom layer requires space below the bottom layer', () => {
     let origin = {key: "0_0", index: 0, layerIndex: 0, relativePosition: 0};
     expect(heightOfEdges([
-    {
-        key: "0_0", index: 0, layerIndex: 0, relativePosition: 0,
-      from: origin,
-      to: {key: "0_1", index: 1, layerIndex: 0, relativePosition: 0}
-    },
-    {
-        key: "0_0", index: 1, layerIndex: 0, relativePosition: 0,
-      from: origin,
-      to: {key: "0_2", index: 2, layerIndex: 0, relativePosition: 0}
-    }
-  ], 1))
-      .toStrictEqual([EDGE_SPACING])
+        {
+            key: "0_0", index: 0, layerIndex: 0, relativePosition: 0,
+            from: origin,
+            to: {key: "0_1", index: 1, layerIndex: 0, relativePosition: 0}
+        },
+        {
+            key: "0_0", index: 1, layerIndex: 0, relativePosition: 0,
+            from: origin,
+            to: {key: "0_2", index: 2, layerIndex: 0, relativePosition: 0}
+        }
+    ], 1))
+        .toStrictEqual([EDGE_SPACING])
 });
 
 test('layers without edges do not require additional height, but will result in an 0 entry', () => {
-    expect(heightOfEdges([
-    ], 4))
+    expect(heightOfEdges([], 4))
         .toStrictEqual([0, 0, 0, 0])
 });
