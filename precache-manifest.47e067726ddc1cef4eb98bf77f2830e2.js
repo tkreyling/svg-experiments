4aa1ed36f1f8a5de2847ff2629e7ea4b0321a1a2
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-01-06 17:05:05 +0100

    Updates

---
+++ precache-manifest.47e067726ddc1cef4eb98bf77f2830e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf186f7f2aae7ca5f7102ea400e0a12",
+    "url": "/svg-experiments/index.html"
+  },
+  {
+    "revision": "3c7a8a710b55c24fb1d9",
+    "url": "/svg-experiments/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3744064e2ec00b60b46",
+    "url": "/svg-experiments/static/js/2.fa23be74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/svg-experiments/static/js/2.fa23be74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c7a8a710b55c24fb1d9",
+    "url": "/svg-experiments/static/js/main.e721e6ac.chunk.js"
+  },
+  {
+    "revision": "d71aa551083c2b7106ad",
+    "url": "/svg-experiments/static/js/runtime-main.01ef7f59.js"
+  }
+]);
