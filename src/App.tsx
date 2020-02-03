320c6a08a3a8f27671457fcac32fed2848971da8
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-03 20:51:02 +0100

    Add function ascending & and for sorting

--- src/App.tsx
+++ src/App.tsx
@@ -412,6 +412,22 @@ function addLayerPositionToEdgeForLayer(edges: Edge<LayerPosition & X>[]) {
     });
 }
 
+export function ascending<T>(property: (obj: T) => number) {
+    return (o1: T, o2: T) => property(o1) - property(o2);
+}
+
+export function and<T>(...sortFunctions: ((o1: T, o2: T) => number)[]) {
+    return (o1: T, o2: T) => {
+        let i = 0;
+        while (true) {
+            if (i == sortFunctions.length) return 0;
+            let result = sortFunctions[i](o1, o2);
+            if (result != 0) return result;
+            i++;
+        }
+    }
+}
+
 function addConnectionIndexAndNumberOfEdgesG<N extends LayerPosition, E, G>(graph: Graph<N, E, G>):
     Graph<N & NumberOfEdges, E & ConnectionIndex, G> {
     addConnectionIndexAndNumberOfEdges(graph.edges);
