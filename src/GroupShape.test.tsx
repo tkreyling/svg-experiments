8924ded95ff19950d2d08285b649fb40081c2d2b
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-04 19:07:38 +0100

    add tests for GroupShape

---
+++ src/GroupShape.test.tsx
@@ -0,0 +1,21 @@
+import {Group, GroupPosition, GroupShape, Height, Node, X, Y} from "./App";
+
+test('coordinates are just passed through', () => {
+  let props: Group<Node, unknown> & GroupPosition & X & Y & Height = {
+    kind: "group",
+    x: 10,
+    y: 10,
+    height: 100,
+    name: "group 1",
+    key: "0_0",
+    index: 0,
+    layerIndex: 0,
+    elements: []
+  };
+
+  let actual = GroupShape(props);
+
+  let rect = actual?.props["children"][0];
+  expect(rect?.props["x"]).toBe(10);
+  expect(rect?.props["y"]).toBe(10);
+});
