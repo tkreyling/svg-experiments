070b7c0136640bbd2ea86224f171869dcbb8ac65
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-11 20:40:54 +0100

    Allow Stack as element in Layer as input for stringsToNodes

--- src/stringsToNodes.test.tsx
+++ src/stringsToNodes.test.tsx
@@ -13,7 +13,7 @@ test('one string results in one node', () => {
 });
 
 test('empty array element result in undefined node', () => {
-    // noinspection JSConsecutiveCommasInArrayLiteral
+    // eslint-disable-next-line no-sparse-arrays
     let input: Group<string | Node, unknown>[][] = [[{name: "group 1", kind: 'group', elements: ["a", , "b"] as any}]];
 
     try {
@@ -42,6 +42,54 @@ test('a node in a layer array is a node', () => {
     let expected: Stack<Node, unknown> = {
         kind: 'stack', elements: [
             {kind: 'layer', elements: [{name: "node", kind: 'node', symbol: "component"}, {name: "group 1", kind: 'group', elements: [{name: "node", kind: 'node'}]}]}
+        ]
+    };
+    expect(stringsToNodes(input)).toStrictEqual(expected);
+});
+
+test('a stack in a layer array is a stack', () => {
+    let input: (string | Node | Group<string | Node, unknown> | Stack<Node, unknown>)[][] =
+        [[{kind: 'stack', elements: []}, {name: "group 1", kind: 'group', elements: ["node"]}]];
+
+    let expected: Stack<Node, unknown> = {
+        kind: 'stack', elements: [
+            {kind: 'layer', elements: [{kind: 'stack', elements: []}, {name: "group 1", kind: 'group', elements: [{name: "node", kind: 'node'}]}]}
+        ]
+    };
+    expect(stringsToNodes(input)).toStrictEqual(expected);
+});
+
+test('a stack with elements keeps its elements', () => {
+    let input: (string | Node | Group<string | Node, unknown> | Stack<Node, unknown>)[][] =
+        [[
+            {
+                kind: 'stack', elements: [{
+                    kind: 'layer',
+                    elements: [{
+                        name: "group 1", kind: 'group', elements: [{name: "node", kind: 'node'}]
+                    }]
+                }]
+            },
+            {name: "group 2", kind: 'group', elements: ["node"]}
+        ]];
+
+    let expected: Stack<Node, unknown> = {
+        kind: 'stack', elements: [
+            {
+                kind: 'layer',
+                elements: [{
+                    kind: 'stack', elements: [{
+                        kind: 'layer',
+                        elements: [{
+                            name: "group 1", kind: 'group', elements: [{name: "node", kind: 'node'}]
+                        }]
+                    }]
+                }, {
+                    name: "group 2",
+                    kind: 'group',
+                    elements: [{name: "node", kind: 'node'}]
+                }]
+            }
         ]
     };
     expect(stringsToNodes(input)).toStrictEqual(expected);
--- src/stringsToNodes.tsx
+++ src/stringsToNodes.tsx
@@ -1,6 +1,8 @@
-import {Group, Node, Stack} from "./graphModel";
+import {Group, Layer, Node, Stack} from "./graphModel";
 
-function convertStringsToNodes(element: (string | Node | Group<string | Node, unknown>)): Node | Group<Node, unknown> {
+function convertStringsToNodes(
+    element: string | Node | Group<string | Node, unknown> | Layer<Node, unknown> | Stack<Node, unknown>
+): Node | Group<Node, unknown> | Layer<Node, unknown> | Stack<Node, unknown> {
     if (typeof element === 'string') {
         return {
             kind: 'node',
@@ -12,10 +14,27 @@ function convertStringsToNodes(element: (string | Node | Group<string | Node, un
         for (let i = 0; i < element.elements.length; i++) {
             if (element.elements[i] === undefined) throw new Error("Empty array elements are not allowed.");
         }
-        return {
-            kind: 'group',
-            name: element.name,
-            elements: element.elements.map(convertStringsToNodes)
+        if ("name" in element) {
+            return {
+                kind: "group",
+                name: element.name,
+                elements: element.elements.map(convertStringsToNodes) as (Group<Node, unknown> | Node)[]
+            }
+        }
+        switch (element.kind) {
+            default:
+            case "stack":  {
+                return {
+                    kind: element.kind,
+                    elements: element.elements.map(convertStringsToNodes) as Layer<Node, unknown>[]
+                }
+            }
+            case "layer": {
+                return {
+                    kind: element.kind,
+                    elements: element.elements.map(convertStringsToNodes) as (Node | Group<Node, unknown> | Stack<Node, unknown>)[]
+                }
+            }
         }
     } else {
         return Object.assign(element, {
@@ -24,13 +43,15 @@ function convertStringsToNodes(element: (string | Node | Group<string | Node, un
     }
 }
 
-export function stringsToNodes(strings: (string | Node | Group<string | Node, unknown>)[][]): Stack<Node, unknown> {
+export function stringsToNodes(
+    strings: (string | Node | Group<string | Node, unknown> | Stack<Node, unknown>)[][]
+): Stack<Node, unknown> {
     return {
         kind: 'stack',
         elements: strings.map(layer => {
             return {
                 kind: 'layer',
-                elements: layer.map(convertStringsToNodes)
+                elements: layer.map(convertStringsToNodes) as (Node | Group<Node, unknown> | Stack<Node, unknown>)[]
             }
         })
     };
