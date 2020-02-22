cd515f4caa28665e659b1ea17feac38a980f08ec
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-22 15:51:05 +0100

    Add optional border for Columns and Rows

--- src/v2/newGraphModel.tsx
+++ src/v2/newGraphModel.tsx
@@ -1,12 +1,17 @@
 export type Node = {
     kind: "node"
 };
+
 export type Row<N> = {
     kind: "row",
+    border?: "solid",
     elements: Element<N>[]
 };
+
 export type Column<N> = {
     kind: "column",
+    border?: "solid",
     elements: Element<N>[]
 };
+
 export type Element<N> = (Node & N) | Row<N> | Column<N>;
