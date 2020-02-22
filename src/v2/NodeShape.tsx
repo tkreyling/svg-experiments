be9ba34742444a44d805a44551aca85c50b149f7
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-22 09:10:52 +0100

    Add NodeShape

---
+++ src/v2/NodeShape.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {OffsetXElements} from "./addOffsetXElements";
+import {OffsetYElements} from "./addOffsetYElements";
+import {ELEMENT_HEIGHT, ELEMENT_WIDTH, HORIZONTAL_SPACING, STROKE_WIDTH, VERTICAL_SPACING} from "../styling";
+
+export const NodeShape: React.FC<OffsetXElements & OffsetYElements> = node => {
+    return (
+        <g key={node.offsetYElements + "_" + node.offsetXElements}>
+            <rect x={node.offsetXElements * (ELEMENT_WIDTH + VERTICAL_SPACING)}
+                  y={node.offsetYElements * (ELEMENT_HEIGHT + HORIZONTAL_SPACING)}
+                  width={ELEMENT_WIDTH}
+                  height={ELEMENT_HEIGHT}
+                  fill="lightgrey" strokeWidth={STROKE_WIDTH} stroke="black"/>
+        </g>
+    );
+};
