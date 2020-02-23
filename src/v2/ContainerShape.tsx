e1ab64d5cf990144bbf15042206933ab052a75be
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-23 21:30:35 +0100

    Adjust border positioning by borderIndexTop, borderIndexMaxTop and borderIndexMaxBottom

--- src/v2/ContainerShape.tsx
+++ src/v2/ContainerShape.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+    BORDER_SPACING_BOTTOM,
+    BORDER_SPACING_TOP,
     BORDER_SPACING_X,
     ELEMENT_HEIGHT,
     ELEMENT_WIDTH,
@@ -14,8 +16,13 @@ import {EmbeddedElementsX} from "./EmbeddedElementsX";
 import {EmbeddedBordersX} from "./EmbeddedBordersX";
 import {BorderIndexMaxX} from "./BorderIndexMaxX";
 import {BorderIndexLeft} from "./BorderIndexLeft";
+import {BorderIndexTop} from "./BorderIndexTop";
+import {BorderIndexMaxBottom} from "./BorderIndexMaxBottom";
+import {BorderIndexMaxTop} from "./BorderIndexMaxTop";
 
-type Props = Container<OffsetElementsX & BorderIndexLeft & OffsetElementsY & EmbeddedElementsX & BorderIndexMaxX & EmbeddedBordersX>;
+type Props = Container<
+    OffsetElementsX & BorderIndexLeft & BorderIndexMaxX & EmbeddedElementsX & EmbeddedBordersX &
+    OffsetElementsY & BorderIndexTop & BorderIndexMaxTop & BorderIndexMaxBottom>;
 
 export const ContainerShape: React.FC<Props> = container => {
     return (
@@ -23,7 +30,10 @@ export const ContainerShape: React.FC<Props> = container => {
             <rect
                 x={container.offsetElementsX * (ELEMENT_WIDTH + HORIZONTAL_SPACING) +
                 (container.borderIndexMaxX * (container.offsetElementsX * 2 + 1)  - container.borderIndexLeft) * BORDER_SPACING_X}
-                y={container.offsetElementsY * (ELEMENT_HEIGHT + VERTICAL_SPACING) + 5}
+                y={container.offsetElementsY * (ELEMENT_HEIGHT + VERTICAL_SPACING)
+                + container.borderIndexMaxTop * (container.offsetElementsY + 1) * BORDER_SPACING_TOP
+                + container.borderIndexMaxBottom * container.offsetElementsY * BORDER_SPACING_BOTTOM
+                - container.borderIndexTop * BORDER_SPACING_TOP}
                 width={container.embeddedElementsX * ELEMENT_WIDTH + (container.embeddedElementsX - 1) * HORIZONTAL_SPACING +
                 ((container.embeddedElementsX - 1) * container.borderIndexMaxX + container.embeddedBordersX) * 2 * BORDER_SPACING_X}
                 height={ELEMENT_HEIGHT}
--- src/v2/styling.tsx
+++ src/v2/styling.tsx
@@ -1,6 +1,8 @@
 export const ELEMENT_WIDTH = 150;
 export const ELEMENT_HEIGHT = 40;
 export const BORDER_SPACING_X = 10;
+export const BORDER_SPACING_TOP = 30;
+export const BORDER_SPACING_BOTTOM = 10;
 export const HORIZONTAL_SPACING = 10;
 export const VERTICAL_SPACING = 20;
 export const STROKE_WIDTH = 0.5;
