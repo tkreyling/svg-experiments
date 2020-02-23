c8b7d9662b7ed78bbea5c3f9b16aeb40d9f9b9d9
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-02-23 19:53:19 +0100

    Add RightBorderIndex

--- src/v2/Diagram.tsx
+++ src/v2/Diagram.tsx
@@ -9,6 +9,7 @@ import {addEmbeddedXElementsG} from "./addEmbeddedXElements";
 import {ContainerShape} from "./ContainerShape";
 import {addMaxEmbeddedXBordersG} from "./addMaxEmbeddedXBorders";
 import {addLeftBorderIndexG} from "./addLeftBorderIndex";
+import {addRightBorderIndexG} from "./addRightBorderIndex";
 
 function allNodes<N>(element: Element<N>): (Node & N)[] {
     switch (element.kind) {
@@ -37,6 +38,7 @@ export const Diagram: React.FC<{element: Element<unknown>}> = props => {
         .map(addOffsetXElementsG)
         .map(addOffsetYElementsG)
         .map(addLeftBorderIndexG)
+        .map(addRightBorderIndexG)
         .map(addEmbeddedXBordersG)
         .map(addMaxEmbeddedXBordersG)
         .map(addEmbeddedXElementsG)
