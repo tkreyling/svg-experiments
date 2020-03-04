caf9e1483f2f0de44a8cd760a4160f84801a9e55
tkreyling <thomas.kreyling@googlemail.com>
2020-03-04 11:32:08 +0000

    Deploying to gh-pages from  - 8961c54b43bd82ba4ddbd932bd5f0fed5a8e2dd6 🚀

---
+++ precache-manifest.4e8b19c041d0e985204d8f55f1f6cc42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99a60a38624c13eae39eccba540fa0c",
+    "url": "/svg-experiments/index.html"
+  },
+  {
+    "revision": "475ddf326e1c34908d66",
+    "url": "/svg-experiments/static/css/main.1d64211b.chunk.css"
+  },
+  {
+    "revision": "860848b8c6366de6d49e",
+    "url": "/svg-experiments/static/js/2.682d76ec.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/svg-experiments/static/js/2.682d76ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "475ddf326e1c34908d66",
+    "url": "/svg-experiments/static/js/main.8aeea6d0.chunk.js"
+  },
+  {
+    "revision": "d71aa551083c2b7106ad",
+    "url": "/svg-experiments/static/js/runtime-main.01ef7f59.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svg-experiments/precache-manifest.b91bf5ce530200ff2bd9682aa73e8645.js"
+  "/svg-experiments/precache-manifest.4e8b19c041d0e985204d8f55f1f6cc42.js"
 );
 
 self.addEventListener('message', (event) => {
