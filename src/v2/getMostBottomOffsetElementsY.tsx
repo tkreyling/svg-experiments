1c1772c023e115b5257cf2221bb57709e41ceb2d
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-03-04 18:34:56 +0100

    Add assertNever for switch cases

--- src/v2/getMostBottomOffsetElementsY.tsx
+++ src/v2/getMostBottomOffsetElementsY.tsx
@@ -1,5 +1,6 @@
 import {Element} from "./newGraphModel";
 import {OffsetElementsY} from "./elementsLayout/OffsetElementsY";
+import {assertNever} from "./assertNever";
 
 export function getMostBottomOffsetElementsY(element: Element<OffsetElementsY>): number {
     switch (element.kind) {
@@ -8,5 +9,8 @@ export function getMostBottomOffsetElementsY(element: Element<OffsetElementsY>):
         case "row":
         case "column":
             return Math.max(...element.elements.map(getMostBottomOffsetElementsY), element.offsetElementsY);
+        default: {
+            assertNever(element);
+        }
     }
 }
