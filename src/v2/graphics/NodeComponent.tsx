f37fcb301efebb9f8d4f30ed87224c777b7f323a
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-03-09 22:20:38 +0100

    Move renderShape to own file

--- src/v2/graphics/NodeComponent.tsx
+++ src/v2/graphics/NodeComponent.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import {
-    DB_CYLINDER_ELLIPSE_Y,
     ELEMENT_HEIGHT,
     ELEMENT_WIDTH,
     QUEUE_CYLINDER_ELLIPSE_X,
-    STROKE_WIDTH,
-    STROKE_WIDTH_SELECTED,
     SYMBOL_SPACING,
     SYMBOL_WIDTH,
     TEXT_PADDING
@@ -15,86 +12,12 @@ import {Node} from "../newGraphModel"
 import {getElementLeftX, RequiredNodeDataGetElementLeftX} from "./getElementLeftX";
 import {getElementTopY, RequiredNodeDataGetElementTopY} from "./getElementTopY";
 import {ComponentSymbol} from "./Symbols";
+import {renderShape} from "./renderShape";
 
 type Props = {
     node : Node & ElementKey & RequiredNodeDataGetElementLeftX & RequiredNodeDataGetElementTopY,
     onNodeClick: (node: Node) => void
 };
-
-function renderShape(
-    shape: "rectangle" | "db-cylinder" | "queue-cylinder" | undefined,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    selected?: boolean,
-    onClick?: () => void
-) {
-    let strokeWidth = selected ? STROKE_WIDTH_SELECTED: STROKE_WIDTH;
-
-    return <>
-        {shape === "rectangle" &&
-        <rect
-            x={x}
-            y={y}
-            width={width}
-            height={height}
-            onClick={onClick}
-            fill="white"
-            strokeWidth={strokeWidth}
-            stroke="black"
-        />
-        }
-        {shape === "db-cylinder" &&
-        <g>
-            <path d={
-                "M " + x + " " + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
-                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,1 " + (x + width) + "," + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
-                "V " + (y + height - DB_CYLINDER_ELLIPSE_Y) + " " +
-                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,1 " + x + "," + (y + height - DB_CYLINDER_ELLIPSE_Y) + " " +
-                "Z"
-            }
-                  onClick={onClick}
-                  stroke="black"
-                  strokeWidth={strokeWidth}
-                  fill="white"
-            />
-            <path d={
-                "M " + x + " " + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
-                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,0 " + (x + width) + "," + (y + DB_CYLINDER_ELLIPSE_Y)
-            }
-                  stroke="black"
-                  strokeWidth={strokeWidth}
-                  fill="none"
-            />
-        </g>
-        }
-        {shape === "queue-cylinder" &&
-        <g>
-            <path d={
-                "M " + (x + QUEUE_CYLINDER_ELLIPSE_X) + " " + y + " " +
-                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,0 " + (x + QUEUE_CYLINDER_ELLIPSE_X) + "," + (y + height) + " " +
-                "H " + (x + width - QUEUE_CYLINDER_ELLIPSE_X) + " " +
-                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,0 " + (x + width - QUEUE_CYLINDER_ELLIPSE_X) + "," + y + " " +
-                "Z"
-            }
-                  onClick={onClick}
-                  stroke="black"
-                  strokeWidth={strokeWidth}
-                  fill="white"
-            />
-            <path d={
-                "M " + (x + QUEUE_CYLINDER_ELLIPSE_X) + " " + y + " " +
-                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,1 " + (x + QUEUE_CYLINDER_ELLIPSE_X) + "," + (y + height)
-            }
-                  stroke="black"
-                  strokeWidth={strokeWidth}
-                  fill="none"
-            />
-        </g>
-        }
-    </>;
-}
 
 export const NodeComponent: React.FC<Props> = props => {
     let node = props.node;
---
+++ src/v2/graphics/renderShape.tsx
@@ -0,0 +1,77 @@
+import {DB_CYLINDER_ELLIPSE_Y, QUEUE_CYLINDER_ELLIPSE_X, STROKE_WIDTH, STROKE_WIDTH_SELECTED} from "../styling";
+import React from "react";
+
+export function renderShape(
+    shape: "rectangle" | "db-cylinder" | "queue-cylinder" | undefined,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    selected?: boolean,
+    onClick?: () => void
+) {
+    let strokeWidth = selected ? STROKE_WIDTH_SELECTED : STROKE_WIDTH;
+
+    return <>
+        {shape === "rectangle" &&
+        <rect
+            x={x}
+            y={y}
+            width={width}
+            height={height}
+            onClick={onClick}
+            fill="white"
+            strokeWidth={strokeWidth}
+            stroke="black"
+        />
+        }
+        {shape === "db-cylinder" &&
+        <g>
+            <path d={
+                "M " + x + " " + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
+                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,1 " + (x + width) + "," + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
+                "V " + (y + height - DB_CYLINDER_ELLIPSE_Y) + " " +
+                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,1 " + x + "," + (y + height - DB_CYLINDER_ELLIPSE_Y) + " " +
+                "Z"
+            }
+                  onClick={onClick}
+                  stroke="black"
+                  strokeWidth={strokeWidth}
+                  fill="white"
+            />
+            <path d={
+                "M " + x + " " + (y + DB_CYLINDER_ELLIPSE_Y) + " " +
+                "A " + (width / 2) + "," + DB_CYLINDER_ELLIPSE_Y + " 0 1,0 " + (x + width) + "," + (y + DB_CYLINDER_ELLIPSE_Y)
+            }
+                  stroke="black"
+                  strokeWidth={strokeWidth}
+                  fill="none"
+            />
+        </g>
+        }
+        {shape === "queue-cylinder" &&
+        <g>
+            <path d={
+                "M " + (x + QUEUE_CYLINDER_ELLIPSE_X) + " " + y + " " +
+                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,0 " + (x + QUEUE_CYLINDER_ELLIPSE_X) + "," + (y + height) + " " +
+                "H " + (x + width - QUEUE_CYLINDER_ELLIPSE_X) + " " +
+                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,0 " + (x + width - QUEUE_CYLINDER_ELLIPSE_X) + "," + y + " " +
+                "Z"
+            }
+                  onClick={onClick}
+                  stroke="black"
+                  strokeWidth={strokeWidth}
+                  fill="white"
+            />
+            <path d={
+                "M " + (x + QUEUE_CYLINDER_ELLIPSE_X) + " " + y + " " +
+                "A " + QUEUE_CYLINDER_ELLIPSE_X + "," + (height / 2) + " 0 1,1 " + (x + QUEUE_CYLINDER_ELLIPSE_X) + "," + (y + height)
+            }
+                  stroke="black"
+                  strokeWidth={strokeWidth}
+                  fill="none"
+            />
+        </g>
+        }
+    </>;
+}
